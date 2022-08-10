1846816d57b1d49eead5cb66c8714ff7d4816468
Daniel Levi-Minzi <daniel.levi-minzi@shipt.com>
2022-08-10 16:03:32 -0400

    all lua

--- nvim/after/plugin/treesitter.rc.lua
+++
@@ -1,7 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  highlight = {
-	enable = true,              
-  },
-}
-require("nvim-treesitter.highlight")
-vim.treesitter.highlighter.hl_map.error = nil
